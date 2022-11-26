3663dd8e7bf3d1c6d236d37f30d7b5c2fe326768
mostafijurmrs <mostafijurrahmanmrs241@gmail.comm>
2022-11-26 15:40:34 +0600

    added a mongodb and create a product collection

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -11,6 +11,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.szoixno.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+
+    finally {
+
+    }
+}
+run().catch(console.log)
 
 
 
